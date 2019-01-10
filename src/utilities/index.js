34f000e5b0278b2f0c39aaed586b0cd7418f72ee
Gajus Kuizinas <gajus@gajus.com>
2019-01-10 14:59:14 +0000

    fix: add missing files

---
+++ src/utilities/index.js
@@ -0,0 +1,3 @@
+// @flow
+
+export {default as isKubernetes} from './isKubernetes';
---
+++ src/utilities/isKubernetes.js
@@ -0,0 +1,6 @@
+// @flow
+
+export default (): boolean => {
+  // eslint-disable-next-line no-process-env
+  return Boolean(process.env.KUBERNETES_SERVICE_HOST);
+};
