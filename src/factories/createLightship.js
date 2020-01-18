0ccae387f79742e70176e6e80c2e51e250558186
Gajus Kuizinas <gajus@gajus.com>
2020-01-18 16:39:08 +0900

    fix: remove unnecessary variable

--- src/factories/createLightship.js
+++ src/factories/createLightship.js
@@ -126,15 +126,15 @@ export default (userConfiguration?: ConfigurationInputType): LightshipType => {
     log.info('received request to shutdown the service');
 
     if (configuration.timeout !== Infinity) {
-      const timeoutId = setTimeout(() => {
+      setTimeout(() => {
         log.warn('timeout occurred before all the shutdown handlers could run to completion; forcing termination');
 
         // eslint-disable-next-line no-process-exit
         process.exit(1);
-      }, configuration.timeout);
+      }, configuration.timeout)
 
-      // $FlowFixMe
-      timeoutId.unref();
+        // $FlowFixMe
+        .unref();
     }
 
     // @see https://github.com/gajus/lightship/issues/12
@@ -187,15 +187,15 @@ export default (userConfiguration?: ConfigurationInputType): LightshipType => {
       }
     });
 
-    const timeoutId = setTimeout(() => {
+    setTimeout(() => {
       log.warn('process did not exit on its own; investigate what is keeping the event loop active');
 
       // eslint-disable-next-line no-process-exit
       process.exit(1);
-    }, 1000);
+    }, 1000)
 
-    // $FlowFixMe
-    timeoutId.unref();
+      // $FlowFixMe
+      .unref();
   };
 
   if (modeIsLocal) {
