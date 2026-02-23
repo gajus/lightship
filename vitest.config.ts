2289a17e8b88424b4e61c689c17a1dd5e439edb1
mlaechele <94380519+mlaechele@users.noreply.github.com>
2026-02-23 16:25:01 +0100

    chore: update and cleanup dependencies

---
+++ vitest.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    coverage: {
+      include: ['src/**/*.ts'],
+      provider: 'v8',
+    },
+    include: ['test/**/*.ts'],
+  },
+});
